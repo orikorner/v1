c2bff8adab8a02324c06d4c55939516e8310e3e2
ori korner <orikorner@gmail.com>
2018-04-16 16:12:46 +0300

    added 2nd activity

---
+++ app/src/main/java/com/example/cabby333/ex1/ChatActivity.java
@@ -0,0 +1,21 @@
+package com.example.cabby333.ex1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+
+}
--- app/src/main/java/com/example/cabby333/ex1/MainActivity.java
+++ app/src/main/java/com/example/cabby333/ex1/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.cabby333.ex1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button chatButton = findViewById(R.id.chatButton);
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_chat);
+            }
+        });
     }
 }
